7c67369b88f3d25ac552180f69d911feeef7f82e
sunguh0904 <sunguh0904@gmail.com>
2024-06-14 12:24:03 +0900

    #5 docs

---
+++ panda-office/src/layouts/Layout.js
@@ -0,0 +1,17 @@
+import Header from "../components/common/Header"
+import Navbar from "../components/common/Navbar"
+import Footer from "../components/common/Footer"
+import { Outlet } from "react-router-dom"
+
+function Layout() {
+    return (
+        <>
+            <Header/>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Layout
