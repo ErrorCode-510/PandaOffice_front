94a6f44eb769f43433c2390870cf685d4ab0a647
NoodleSh <na619620@naver.com>
2024-06-21 12:20:22 +0900

    #15-statistics second commit

--- panda-office/src/App.js
+++ panda-office/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Main from "./pages/main/Main"
 import Layout from "./layouts/Layout"
+import Statistics from "./pages/welfare/statistics/Statistics.js"
 import LoginForm from "./components/Assets/LoginForm/LoginForm.jsx"
-import "./style.css"
+import "./pages/main/Main.css"
+import "./pages/welfare/statistics/Statistics.css"
+import "./common.css"
 
 
 
@@ -11,11 +14,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LoginForm/>}>
-          <Route path='/' element={<Layout/>}>
-              <Route index element={<Main/>}/>
+        {/* <Route path='/' element={<LoginForm/>}> */}
+            <Route path='/' element={<Layout/>}>
+                <Route index element={<Main/>}/>
+                {/* <Route index element={<Statistics/>}/> */}
             </Route>
-        </Route>
+        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   )
