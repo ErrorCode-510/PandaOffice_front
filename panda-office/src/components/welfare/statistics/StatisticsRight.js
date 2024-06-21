9291e2464ad17d76e950d11a8b114ad444930108
NoodleSh <na619620@naver.com>
2024-06-21 12:13:42 +0900

    #15 statistics commit

---
+++ panda-office/src/components/welfare/statistics/StatisticsRight.js
@@ -0,0 +1,11 @@
+function StaticsRight() {
+    return(
+        <>
+             <div className="statistics-right-card">
+                    오른쪽은 따로 컴포넌트 만드는게 나을듯 
+            </div>
+        </>
+    )
+}
+
+export default StaticsRight;
---
+++ panda-office/src/pages/welfare/statistics/Statistics.js
@@ -0,0 +1,41 @@
+import Footer from "../../../components/common/Footer"
+import StatisticsLeft from "../../../components/welfare/statistics/StatisticsLeft"
+import StatisticsRight from "../../../components/welfare/statistics/StaticsRight"
+
+
+function Statistics() {
+    
+    // 좌측 구역`
+    const statisticsLeftMaterials = {
+        classNames:["all-respondent", "respondent-percent", "rank-response-result", "all-response-percent"],
+        titles:["총 응답자수","직급별 응답","직급별 응답 결과","가장 많은 응답 결과"]
+    }
+
+
+    return(
+        <>
+            <div className="statistics-area">
+                <div className="cover-box">
+                    <div className=" statistics-inner-area"> 
+                        <div className="statistics-inner-left-area">
+                            {
+                                statisticsLeftMaterials.classNames.map((className,index)=>
+                                    <StatisticsLeft key={index} className={className} title={statisticsLeftMaterials.titles[index]}/>
+                                )
+                            }
+                            
+                            
+                        </div>
+                        <div className="statistics-inner-right-area">
+                            <StatisticsRight/>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+
+            <Footer/>
+        </>
+    )
+}
+
+export default Statistics;
