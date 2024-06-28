1e9bca6b3149a07207a73345304d5d58fcd70e38
philosophosDeka <gosme3@gmail.com>
2024-06-28 17:17:29 +0900

    #18 feat 급여개인조회

--- panda-office/src/pages/Employee/EmployeeRoutes.js
+++ panda-office/src/pages/Employee/EmployeeRoutes.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import EmployeeLayout from "./EmployeeLayout"
+import MyPay from './Payroll/MyPay';
+import EmplPayroll from './Payroll/EmplPayroll';
 
 function EmployeeRoutes() {
     return (
         <Routes>
-            <Route path="test" element={<div>인사관리 라우팅 테스트</div>} />
-            {/* 라우트 적용 */}
+            <Route index="/employee" element={<EmployeeLayout />} />
+            <Route path="payroll/MyPay" element={<MyPay />} />
+            <Route path="payroll/EmplPayroll" element={<EmplPayroll />} />
         </Routes>
     );
 }
