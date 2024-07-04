a759b25b9ea4773dd42ae2acf0b0a7f6614d7740
sunguh0904 <sunguh0904@gmail.com>
2024-07-04 15:39:54 +0900

    라우트 및 사이드바 페이지 경로 연결

---
+++ panda-office/src/pages/recruitment/schedule/Schedule.js
@@ -0,0 +1,9 @@
+const Schedule = () => {
+    return (
+        <>
+            면접일정 테스트
+        </>
+    )
+}
+
+export default Schedule;
---
+++ panda-office/src/pages/recruitment/schedule/ScheduleCreate.js
@@ -0,0 +1,9 @@
+const ScheduleCreate = () => {
+    return (
+        <>
+            면접일정 생성 테스트
+        </>
+    )
+}
+
+export default ScheduleCreate;
