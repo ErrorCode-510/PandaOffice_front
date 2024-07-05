7beea748696241bbc0649adae705ec0644ad4d71
KMJ90 <kst906388@gmail.com>
2024-07-05 21:28:24 +0900

    게시글 조회 및 게시글 상세조회 구현

--- panda-office/src/apis/NoticeAPICalls.js
+++ panda-office/src/apis/NoticeAPICalls.js
@@ -1,12 +1,13 @@
 import { authRequest } from './api';
-import { getNotice, setNotice } from '../modules/NoticeModules';
-
+import { getNotice, getNoticeByCategory, setNotice } from '../modules/NoticeModules';
 
 /* 공지사항 전체 조회 API */
 export const callNoticeListAPI = ({ currentPage }) => {
     return async (dispatch, getState) => {
+
         const result = await authRequest.get(`/notice/notices?page=${currentPage}`);
-        console.log("test" + JSON.stringify(currentPage))
+        console.log("Fetching all notices for page: " + currentPage);
+
         if (result.status === 200) {
             dispatch(getNotice(result));
         } else {
@@ -15,16 +16,28 @@ export const callNoticeListAPI = ({ currentPage }) => {
     };
 };
 
-
 /* 공지사항 상세 조회 API */
 export const callNoticeDetailAPI = (noticeId) => {
     return async (dispatch, getState) => {
-        const result = await authRequest.get(`/notice/detail/${noticeId}`);
-
+        
+        const result = await authRequest.get(`/notice/notices/${noticeId}`);
         if (result.status === 200) {
             dispatch(setNotice(result));
         } else {
             console.error('callNoticeDetailAPI error : ', result);
+        }
+    };
+}
+
+/* 카테고리 별 공지사항 조회 API */
+export const callNoticeByCategoryAPI = ({ category, subCategory, currentPage }) => {
+    return async (dispatch, getState) => {
+        const result = await authRequest.get(`/notice/category/${category}/${subCategory}?page=${currentPage}`);
+        console.log(`Fetching notice for category: ${category}, subCategory: ${subCategory}, page: ${currentPage}`);
+        if (result.status === 200) {
+            dispatch(getNoticeByCategory(result));
+        } else {
+            console.error('callNoticeByCategoryAPI error : ', result);
         }
     };
 }
--- panda-office/src/pages/notice/NoticeList.js
+++ panda-office/src/pages/notice/NoticeList.js
@@ -1,11 +1,36 @@
 import NoticeListItem from "./NoticeListItem";
-import './notice.css';  // css 파일을 임포트
-import React from "react";
+import './notice.css';
+import { callNoticeListAPI, callNoticeByCategoryAPI } from "../../apis/NoticeAPICalls";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 const NoticeList = ({notice}) => {
-    {/* console.log("공지사항 리스트 데이터 받기 : " + JSON.stringify((notice))) */ }
+    const { category, subCategory } = useParams();
+    const dispatch = useDispatch();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
+    // useEffect(() => {
+    //     if (category && subCategory) {
+    //         dispatch(callNoticeByCategoryAPI({ category, subCategory, currentPage }));
+    //     } else {
+    //         dispatch(callNoticeListAPI({ currentPage }));
+    //     }
+    // }, [category, subCategory, currentPage, dispatch]);
+
+    // useEffect(() => {
+    //     if (notice && notice.pagingButtonInfo) {
+    //         setTotalPages(notice.pagingButtonInfo.totalPages);
+    //     }
+    // }, [notice]);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
     return (
-        <>
+        <div className="notice-wrap">
             <div className="noticeList-ui">
                 <ul className="noticeList-title">
                     <li>번호</li>
@@ -20,8 +45,18 @@ const NoticeList = ({notice}) => {
                     <NoticeListItem key={notice.noticeId} notice={notice} />
                 ))}
             </div>
-        </>
-    )
-}
+            {/* <div className="pagination">
+                {Array.from({ length: totalPages }, (_, index) => (
+                    <button
+                        key={index}
+                        onClick={() => handlePageChange(index + 1)}
+                        disabled={currentPage === index + 1}
+                    >
+                    </button>
+                ))}
+            </div> */}
+        </div>
+    );
+};
 
 export default NoticeList;
