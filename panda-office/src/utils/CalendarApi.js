d48b219887f29b0c446a417c2fbafc6f77c11a74
philosophosDeka <gosme3@gmail.com>
2024-07-10 12:36:27 +0900

    #72 update 급여 등록

--- panda-office/src/utils/CalendarApi.js
+++ panda-office/src/utils/CalendarApi.js
@@ -3,6 +3,10 @@ import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid";
 
 function CalendarApi({ height, headerToolbar }) {
+    // 날짜 셀의 내용을 커스터마이즈하는 함수
+    const customDayCellContent = ({ date }) => {
+        return <span>{date.getDate()}</span>; // 날짜에서 일(날짜 숫자)만 출력
+    };
     return (
         <div>
             <FullCalendar
@@ -17,6 +21,8 @@ function CalendarApi({ height, headerToolbar }) {
                 editable={true}
                 /* 여러 날짜 선택 가능 */
                 selectable={true}
+                // 날짜 셀의 내용을 커스터마이즈하는 함수를 전달
+                dayCellContent={customDayCellContent} 
             />
         </div>
     )
